02a29e1034899823b254a724f3492ab96017727b
Joe Kerrigan <joek1301@gmail.com>
2022-05-15 21:34:14 -0400

    Add dummy CRUD endpoints for boards
    - Add new (dummy) endpoints for boards: create_board, get_board,
      update_board, and delete_board

--- backend/handler.py
+++ backend/handler.py
@@ -1,23 +1,49 @@
 import json
+import base64
 import os
 
+# name of the s3 bucket we store boards in
+BOARDS_BUCKET_NAME = os.environ.get("BOARDS_BUCKET_NAME")
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
-        "bucket_name": os.environ.get("BOARDS_BUCKET_NAME", ""),
-    }
+# raise error if missing
+if BOARDS_BUCKET_NAME is None:
+    raise RuntimeError("Missing required environment variable 'BOARDS_BUCKET_NAME'")
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
+def _get_body(event):
+    """Gets request body, base64 decodes if necessary,
+    and parses JSON into a Python dict.
     """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    # get POST body as a string
+    body = event["body"]
+
+    # check if the POST body string is base64-encoded
+    if event["isBase64Encoded"]:
+        # decode it
+        body = base64.b64decode(body)
+
+    # parse into a python object
+    return json.loads(body)
+
+
+def create_board(event, context):
+    body = {"hello": "world"}
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def update_board(event, context):
+    board_id = event["pathParameters"]["board_id"]
+    body = {"hello": "world", "board_id": board_id}
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def get_board(event, context):
+    board_id = event["pathParameters"]["board_id"]
+    body = {"hello": "world", "board_id": board_id}
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def delete_board(event, context):
+    board_id = event["pathParameters"]["board_id"]
+    body = {"hello": "world", "board_id": board_id}
+    return {"statusCode": 200, "body": json.dumps(body)}
